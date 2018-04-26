91310eb49bdd8bda4e0e35ccbe069b571a7fe475
François Samaké <francois.samake@zenika.com>
2018-04-26 16:56:27 +0200

    Set Event entity

--- src/main/java/fr/icchoirlyon/event/EventRepository.java
+++ src/main/java/fr/icchoirlyon/event/EventRepository.java
@@ -1,4 +1,11 @@
 package fr.icchoirlyon.event;
 
-public interface EventRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Long>{
+    List<Event> findall();
 }
